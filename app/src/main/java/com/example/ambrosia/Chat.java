be9e6483df529a47e909dba8d828b1b4382fc50d
Audoux Nicolas <nicolas.audoux13@gmail.com>
2022-04-20 14:57:14 +0200

    Créer un évènement dans l'agenda

--- app/src/main/java/com/example/ambrosia/Chat.java
+++ app/src/main/java/com/example/ambrosia/Chat.java
@@ -1,26 +1,14 @@
 package com.example.ambrosia;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class Forum extends Fragment {
-    @Override
+public class Chat {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forum, container, false);
-
+        return inflater.inflate(R.layout.fragment_chat, container, false);
     }
-
-
-
 }
--- app/src/main/java/com/example/ambrosia/ParametreProfil.java
+++ app/src/main/java/com/example/ambrosia/ParametreProfil.java
@@ -1,14 +1,54 @@
 package com.example.ambrosia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ParametreProfil extends AppCompatActivity {
-
+    public Calendar calendar;
+    private DatePicker datePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametre_profil);
+        datePicker= (DatePicker) this.findViewById(R.id.myCalendar);
+        calendar=Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        int year = calendar.get(Calendar.YEAR);
+        int month  = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        this.datePicker.init( year, month , day , new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                setTime( year,   month,   dayOfMonth);
+            }
+        });
+    }
+
+    public void onClick(View view){
+        long time = calendar.getTime().getTime();
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra("beginTime",time);
+        intent.putExtra("allDay", true);
+        intent.putExtra("rule", "FREQ=YEARLY");
+        intent.putExtra("endTime", time+60*60*1000);
+        intent.putExtra("title", "A Test Event from android app");
+        startActivity(intent);
+    }
+
+    public void setTime(int year, int month, int dayOfMonth){
+        calendar = new GregorianCalendar(year, month, dayOfMonth);
     }
 }
