3210663689425b79bc935ea88746f51f9ed7be3a
Matthieu Féraud <matthieuferaud31@gmail.com>
2022-04-26 23:52:28 +0200

    parcelable

--- app/src/main/java/com/example/ambrosia/planning/Day/Day.java
+++ app/src/main/java/com/example/ambrosia/planning/Day/Day.java
@@ -1,13 +1,18 @@
 package com.example.ambrosia.planning.Day;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.ambrosia.R;
+import com.example.ambrosia.planning.Details.Details;
+import com.example.ambrosia.planning.Food;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,5 +24,10 @@ public class Day extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_day, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
--- app/src/main/java/com/example/ambrosia/planning/Details/Details.java
+++ app/src/main/java/com/example/ambrosia/planning/Details/Details.java
@@ -1,27 +1,26 @@
 package com.example.ambrosia.planning.Details;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.ambrosia.R;
+import com.example.ambrosia.planning.Food;
 
-public class Details extends Fragment {
+public class Details extends Activity {
+    private Food food;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       //getActivity().getIntent().getParcelableExtra(getString(R.string.))
-    }
+        food = getIntent().getParcelableExtra("food");
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        Log.d("Parcelable", food.getName());
     }
 }
