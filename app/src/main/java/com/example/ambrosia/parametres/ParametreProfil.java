539d0054040756e9f246ce192e9a7159c969f8a8
Valentin-nasone <valentin.nasone@etu.unice.fr>
2022-04-26 23:39:03 +0200

    rpivbez

--- app/src/main/java/com/example/ambrosia/parametres/ParametreProfil.java
+++ app/src/main/java/com/example/ambrosia/parametres/ParametreProfil.java
@@ -5,25 +5,34 @@ import androidx.fragment.app.Fragment;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.ambrosia.R;
+import com.example.ambrosia.Users.User;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
 
 public class ParametreProfil extends Fragment {
     public Calendar calendar;
     private DatePicker datePicker;
+    public User user = new User();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         View myView = inflater.inflate(R.layout.fragment_profile_name, container, false);
         ImageView button = (ImageView) myView.findViewById(R.id.imageAnniv);
         button.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +48,33 @@ public class ParametreProfil extends Fragment {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(view.getContext(),
                         dateSetListener, 2000, 00, 1);
                 datePickerDialog.show();
+
+            }
+        });
+
+        EditText lasts = (EditText) myView.findViewById(R.id.lastName);
+        EditText firsts = (EditText) myView.findViewById(R.id.firstName);
+        // RadioGroup sexes = (RadioGroup) myView.findViewById(R.id.radioGroup);
+
+        Button creationProfil = (Button) myView.findViewById(R.id.buttonValidation);
+        EditText emails = (EditText) myView.findViewById(R.id.email);
+        EditText mdps = (EditText) myView.findViewById(R.id.mot_de_passe);
+
+        creationProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String last = lasts.getText().toString();
+                String first = firsts.getText().toString();
+                String email = emails.getText().toString();
+                String mdp = mdps.getText().toString();
+
+                user.setMail(email);
+                user.setFirst(first);
+                user.setLast(last);
+                user.setPassword(mdp);
+
+                db.collection("user").document(user.getMail()).set(user);
+
 
             }
         });
--- app/src/main/java/com/example/ambrosia/parametres/ParametreProfilBis.java
+++ app/src/main/java/com/example/ambrosia/parametres/ParametreProfilBis.java
@@ -1,20 +1,28 @@
 package com.example.ambrosia.parametres;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.ambrosia.R;
+import com.example.ambrosia.Users.User;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ParametreProfilBis extends Fragment {
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_profile_mail, container, false);
+
         return myView;
     }
 }
